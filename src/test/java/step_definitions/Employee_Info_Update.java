5e90bceb7b9c3374f898c35d339746c50445e0e3
unknown <shabana237@gmail.com>
2023-06-26 12:40:48 +0600

    print stmt added

--- src/test/java/step_definitions/Employee_Info_Update.java
+++ src/test/java/step_definitions/Employee_Info_Update.java
@@ -89,6 +89,7 @@ public class Employee_Info_Update extends Base{
 		
 		String s= getAttributeValue(By.xpath("//input[@name='contact']"), "value" );
 	    assertEquals("333333",s); 
+	    System.out.println(s);
 	}
 
 }
